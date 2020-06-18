fd70871000a2915a3046e72c33cfdf23c059be37
element222 <fabian.machicao@gmail.com>
2020-06-18 17:44:49 -0400

    Terminado

--- app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
+++ app/src/main/java/bo/upb/programacion3/codelabpokedex/utils/PokemonUtils.java
@@ -27,6 +27,17 @@ public class PokemonUtils {
         pokemons.add(new Pokemon(10, "Sandshrew", R.drawable.sandshrew, "Ground"));
         pokemons.add(new Pokemon(11, "Venonat", R.drawable.venonat, "Bug"));
         pokemons.add(new Pokemon(12, "Zubat", R.drawable.zubat, "Poison"));
+        pokemons.add(new Pokemon(13, "Arceus", R.drawable.arceus, "Legendary"));
+        pokemons.add(new Pokemon(14, "Chimchar", R.drawable.chimchar, "Fire"));
+        pokemons.add(new Pokemon(15, "Dialga", R.drawable.dialga, "Legendary"));
+        pokemons.add(new Pokemon(16, "Sudowoodo", R.drawable.sudowoodo, "Rock"));
+        pokemons.add(new Pokemon(17, "Glaiscor", R.drawable.glaiscor, "Dark"));
+        pokemons.add(new Pokemon(18, "Onix", R.drawable.onix, "Rock"));
+        pokemons.add(new Pokemon(19, "Darkrai", R.drawable.darkrai, "Legendary"));
+        pokemons.add(new Pokemon(20, "Alakazam", R.drawable.alakazam, "Physic"));
+        pokemons.add(new Pokemon(21, "Regigigas", R.drawable.regigigas, "Lengendary"));
+        pokemons.add(new Pokemon(22, "Cacnea", R.drawable.cacnea, "Grass"));
+
         return pokemons;
     }
 }
